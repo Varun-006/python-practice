5fd0df745923edaed9178120da012cdbed077c8f
VARUNA D <varund9110820617@gmail.com>
2026-02-12 23:24:54 +0530

    Add files via upload
    Python has built-in data types like numeric, string, list, tuple, set, dictionary, boolean, and NoneType. These define the type of value stored in a variable

---
+++ DataTypes.py
@@ -0,0 +1,27 @@
+#python is dynamic programming language so we dont need to declare the variable type
+#Python has built-in data types like numeric, string, list, tuple, set, dictionary, boolean, and NoneType. These define the type of value stored in a variable
+#1. int
+x=10
+print(x)
+#2. float
+y=3.14
+print(y)
+#3. str
+name="python"
+print(name)
+#4. bool
+i_am_a_student=True
+print(i_am_a_student)
+#5. list &list is mutable
+my_colors=["red","green","blue"]
+print(my_colors)
+#6. tuple and tuple is immutable
+my_tuple=(1,2,3)
+print(my_tuple)
+#7. set
+my_set={1,2,3}
+print(my_set)
+#8. dict
+my_dict={"name":"python","version":3.8}
+print(my_dict)
+#note:list,tuple,set and dict are collection data types in python
